8fb16eabca5518158adf9220a2102575aa71fe8e
hojae29 <lhj@jwins.co.kr>
2025-12-11 13:58:52 +0900

    94. State(상태) 끌어올리기 대안

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -94,15 +94,18 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
-        {winner ||
-          (hasDraw && <GameOver winner={winner} onRestart={handleRestart} />)}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
         <GameBoard onSelectSqure={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
